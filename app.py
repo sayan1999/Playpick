6d10d5bfc6d663737860f0e99aa5bedbd4b3928a
sayan1999 <mr.sayan.dey@gmail.com>
2023-12-16 00:24:10 +0530

    analytics

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from streamlit_star_rating import st_star_rating
 import imdb, glob
 import datetime
 from db import DB
+import streamlit-analytics
 
 
 N_REC = 40
@@ -130,143 +131,148 @@ def get_cover_link(movie):
 
 
 if __name__ == "__main__":
-    print("*" * 100, "\n")
-    st.set_page_config(layout="wide", page_title="PlayPick")
-    st.title("PlayPick")
-    st.write(
-        "**Confused? Which movie to watch next? I am PlayPick, I shall help you pick the best movie to watch next for You.**"
-    )
+    with streamlit_analytics.track(unsafe_password="credict123"):
+        print("*" * 100, "\n")
+        st.set_page_config(layout="wide", page_title="PlayPick")
+        st.title("PlayPick")
+        st.write(
+            "**Confused? Which movie to watch next? I am PlayPick, I shall help you pick the best movie to watch next for You.**"
+        )
 
-    IMDB, db, corr, moviesinfo = init()
+        IMDB, db, corr, moviesinfo = init()
 
-    #  *********************************** User authentication ***********************************************
-    user = st.selectbox(
-        "Select an account",
-        ["Choose", "Create account"] + get_users(),
-        placeholder="Select User",
-    )
-    if user not in ["", "Choose"]:
-        if user == "Create account":
-            placeholder = st.empty()
-            with placeholder.container():
-                newuser = st.text_input("Enter your name", max_chars=20).strip()
-                newpass = st.text_input(
-                    "Enter password", type="password", max_chars=20
-                ).strip()
-                submituser = st.button("Submit")
-                if submituser and newuser:
-                    err, sts = create_user(newuser, newpass)
-                    if sts:
-                        user = newuser
-                        st.text("User account created, refresh to start now")
-                        if st.button("Refresh"):
-                            placeholder.empty()
-                    else:
-                        st.text(err)
-            pass
-        else:
-            passw = st.text_input("Enter password", type="password").strip()
-            if validate_passw(user, passw):
-                #  *********************************** authentication done, recommendation now ***********************************************
-
-                # ******************************* manual search ***************************
-                selected = st.selectbox(
-                    "Tell us movies you have watched",
-                    [""] + get_movies(),
-                    placeholder="Select movie",
-                )
-                if selected:
-                    rate = st.number_input(
-                        f"Rate {selected} 1-5", min_value=1, max_value=5, value=None
-                    )
-                    if rate:
-                        add_user_movies(user, [(selected, rate)])
-
-                placeholder2 = st.empty()
-                with placeholder2.container():
-                    st.text(
-                        "Mark the Movies you have watched and we'll improve our recommendation"
-                    )
-                    with st.form(
-                        "recommendations",
-                    ):
-                        if not to_refresh():
-                            print("Serving cached results...")
-                            st.session_state["movies"] = st.session_state.get(
-                                "movies", get_user_movies(user)
-                            )
-
+        #  *********************************** User authentication ***********************************************
+        user = st.selectbox(
+            "Select an account",
+            ["Choose", "Create account"] + get_users(),
+            placeholder="Select User",
+        )
+        if user not in ["", "Choose"]:
+            if user == "Create account":
+                placeholder = st.empty()
+                with placeholder.container():
+                    newuser = st.text_input("Enter your name", max_chars=20).strip()
+                    newpass = st.text_input(
+                        "Enter password", type="password", max_chars=20
+                    ).strip()
+                    submituser = st.button("Submit")
+                    if submituser and newuser:
+                        err, sts = create_user(newuser, newpass)
+                        if sts:
+                            user = newuser
+                            st.text("User account created, refresh to start now")
+                            if st.button("Refresh"):
+                                placeholder.empty()
                         else:
-                            print("New results...")
-                            st.session_state["movies"] = get_user_movies(user)
-
-                        rec = get_recommendation(st.session_state["movies"])
-
-                        selections = []
-                        stars = []
-                        thumbnails = []
-                        thumbnail_link = []
-
-                        col1, col2, col3, col4, col5 = st.columns([3, 4, 1, 1, 5])
-                        col1.text("Title")
-                        col2.text("Genre")
-                        col3.write("IMDB Rating")
-                        col4.write("IMDB rating")
-                        col5.write("Your rating")
-                        for i, (movie, genre, rating, reviews) in enumerate(
-                            rec.reset_index().values.tolist()
-                        ):
-                            # print("Debug", st.session_state)
-                            if i == len(rec) - N_RANDOM:
-                                st.markdown("Some random recommendations as well...")
-                            col1, col2, col3, col4, col5 = st.columns([3, 4, 1, 1, 5])
-                            with col1:
-                                selections.append(
-                                    st.checkbox(
-                                        movie,
-                                        key="Checkbox_" + movie,
-                                    )
-                                )
-                            col2.markdown(genre)
-                            col3.text(round(rating, 1))
-                            col4.write(int(reviews))
-                            with col5:
-                                stars.append(
-                                    st_star_rating(
-                                        "",
-                                        5,
-                                        5,
-                                        size=15,
-                                        read_only=False,
-                                        key="Star_" + movie,
-                                    )
-                                )
-                            # thumbnails.append(st.empty())
-                            # thumbnail_link.append(movie)
-
-                        form_submitted = st.form_submit_button("Submit")
-
-                if form_submitted:
-                    add_user_movies(
-                        user,
-                        [
-                            (rec.index.tolist()[i], stars[i])
-                            for i, sel in enumerate(selections)
-                            if sel
-                        ],
-                    )
-                    placeholder2.empty()
-                    st.text(
-                        "Submitted your watched movies, refresh to get fresh recommendations..."
-                    )
-
-                recommend = st.button("Recommend")
-                st.text("Movies watched by you...")
-                st.write(str(get_user_movies(user)))
-
-                # for plc, movie in zip(thumbnails, thumbnail_link):
-                #     if get_cover_link(movie):
-                #         plc.image(get_cover_link(movie))
-
+                            st.text(err)
+                pass
             else:
-                st.text("Authentication failed")
+                passw = st.text_input("Enter password", type="password").strip()
+                if validate_passw(user, passw):
+                    #  *********************************** authentication done, recommendation now ***********************************************
+
+                    # ******************************* manual search ***************************
+                    selected = st.selectbox(
+                        "Tell us movies you have watched",
+                        [""] + get_movies(),
+                        placeholder="Select movie",
+                    )
+                    if selected:
+                        rate = st.number_input(
+                            f"Rate {selected} 1-5", min_value=1, max_value=5, value=None
+                        )
+                        if rate:
+                            add_user_movies(user, [(selected, rate)])
+
+                    placeholder2 = st.empty()
+                    with placeholder2.container():
+                        st.text(
+                            "Mark the Movies you have watched and we'll improve our recommendation"
+                        )
+                        with st.form(
+                            "recommendations",
+                        ):
+                            if not to_refresh():
+                                print("Serving cached results...")
+                                st.session_state["movies"] = st.session_state.get(
+                                    "movies", get_user_movies(user)
+                                )
+
+                            else:
+                                print("New results...")
+                                st.session_state["movies"] = get_user_movies(user)
+
+                            rec = get_recommendation(st.session_state["movies"])
+
+                            selections = []
+                            stars = []
+                            thumbnails = []
+                            thumbnail_link = []
+
+                            col1, col2, col3, col4, col5 = st.columns([3, 4, 1, 1, 5])
+                            col1.text("Title")
+                            col2.text("Genre")
+                            col3.write("IMDB Rating")
+                            col4.write("IMDB rating")
+                            col5.write("Your rating")
+                            for i, (movie, genre, rating, reviews) in enumerate(
+                                rec.reset_index().values.tolist()
+                            ):
+                                # print("Debug", st.session_state)
+                                if i == len(rec) - N_RANDOM:
+                                    st.markdown(
+                                        "Some random recommendations as well..."
+                                    )
+                                col1, col2, col3, col4, col5 = st.columns(
+                                    [3, 4, 1, 1, 5]
+                                )
+                                with col1:
+                                    selections.append(
+                                        st.checkbox(
+                                            movie,
+                                            key="Checkbox_" + movie,
+                                        )
+                                    )
+                                col2.markdown(genre)
+                                col3.text(round(rating, 1))
+                                col4.write(int(reviews))
+                                with col5:
+                                    stars.append(
+                                        st_star_rating(
+                                            "",
+                                            5,
+                                            5,
+                                            size=15,
+                                            read_only=False,
+                                            key="Star_" + movie,
+                                        )
+                                    )
+                                # thumbnails.append(st.empty())
+                                # thumbnail_link.append(movie)
+
+                            form_submitted = st.form_submit_button("Submit")
+
+                    if form_submitted:
+                        add_user_movies(
+                            user,
+                            [
+                                (rec.index.tolist()[i], stars[i])
+                                for i, sel in enumerate(selections)
+                                if sel
+                            ],
+                        )
+                        placeholder2.empty()
+                        st.text(
+                            "Submitted your watched movies, refresh to get fresh recommendations..."
+                        )
+
+                    recommend = st.button("Recommend")
+                    st.text("Movies watched by you...")
+                    st.write(str(get_user_movies(user)))
+
+                    # for plc, movie in zip(thumbnails, thumbnail_link):
+                    #     if get_cover_link(movie):
+                    #         plc.image(get_cover_link(movie))
+
+                else:
+                    st.text("Authentication failed")
